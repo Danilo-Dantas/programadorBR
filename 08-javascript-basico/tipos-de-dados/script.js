600d90b7e79cfa09634011fcd2ee43e97db54e1d
Danilo Dantas <danilodantasofc@gmail.com>
2022-04-27 02:32:47 -0300

    tipos de dados

---
+++ 08-javascript-basico/tipos-de-dados/script.js
@@ -0,0 +1,18 @@
+// Tipos de variáveis
+
+// Boolean
+// Null
+// Undefined
+// Number
+// String
+
+// Tudo que estiver na linha vai ser um comentário.
+
+/* 
+  Tudo que estiver no bloco vai ser comentário.
+*/
+
+var a = "Texto string" // String
+var b = 3              // Number
+var c                  // Undefined
+var d = null           // Null
