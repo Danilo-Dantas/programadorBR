267a49b226d767aed1d8bc113ac8f49191432f3c
Danilo Dantas <danilodantasofc@gmail.com>
2022-05-03 17:37:30 -0300

    treino de alert e prompt

---
+++ 08-javascript-basico/alert-prompt/script.js
@@ -0,0 +1,11 @@
+// alert("bom dia, Danilo!")
+
+// var nome = prompt("Qual o seu nome?")
+
+// alert("Bom dia, " + nome)
+
+var numero = prompt("Digite um número")
+
+var dobro = parseInt(numero) + parseInt(numero)
+
+alert("O dobro do número que você digitou é " + dobro)
