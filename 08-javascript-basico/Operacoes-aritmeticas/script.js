a455da8f5b982d299ab7b87ba8a3f91ed2c09e5c
Danilo Dantas <danilodantasofc@gmail.com>
2022-05-03 17:33:30 -0300

    parse Int/float

---
+++ 08-javascript-basico/Operacoes-aritmeticas/script.js
@@ -0,0 +1,18 @@
+// + Adição
+// - Subtração
+// * Multiplicação
+// / Divisão
+// % módulo (Resto da divisão)
+// ++ Incrementação
+// -- Decremento
+
+var a = 10
+var b = 15
+
+var c = a + b
+
+a = a + 1
+//  ou
+a++
+
+console.log(c)
