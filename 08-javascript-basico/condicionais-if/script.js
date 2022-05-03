ff53de78c154dacc283758eeefd9cd36a156a6f5
Danilo Dantas <danilodantasofc@gmail.com>
2022-05-03 17:48:11 -0300

    treino de if

---
+++ 08-javascript-basico/condicionais-if/script.js
@@ -0,0 +1,13 @@
+var idade = 77
+
+if (idade <= 18 && idade <= 70) {
+  console.log("Pode comprar")
+  console.log("Qual o seu pedido?")
+}
+else {
+  console.log("Não pode")
+  console.log("Volte futuramente.")
+}
+
+
+
